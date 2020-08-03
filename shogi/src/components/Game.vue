b801f487f9c4670e63e161a5815425516869342f
t-akzw <45358756+t-akzw@users.noreply.github.com>
2020-08-03 23:49:01 +0900

    とりあえずv-row, v-colでコマ情報の文字列を配置することに成功

--- shogi/src/components/Game.vue
+++ shogi/src/components/Game.vue
@@ -2,11 +2,13 @@
   <div class="Top">
   <div class="Game">Game {{ $route.params.id }}</div>
 
-  <div id="board">{{ board[1]["ich"] }}
-    <div id="itemA">A</div> <!-- アイテム -->
-    <div id="itemB">B</div> <!-- アイテム -->
-    <div id="itemC">C</div> <!-- アイテム -->
-  </div>
+  <v-container>
+    <v-row justify="center" v-for="(i, idx_row) in board" :key="idx_row" >
+      <v-col v-for="(j, idx_col) in i" :key="idx_col" >
+        {{ j }}
+      </v-col>
+    </v-row>
+  </v-container>
 
   </div>
 </template>
@@ -16,25 +18,31 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Game extends Vue {
-  board = {
-    1: {
-      ich: "香車"
-    }
-  }
-
+  board = [
+    ["香車", "桂馬", "銀将", "金将", "王将", "金将", "銀将", "桂馬", "香車"],
+    ["", "飛車", "", "", "", "", "", "角行", ""],
+    ["歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵"],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵", "歩兵"],
+    ["", "角行", "", "", "", "", "", "飛車", ""],
+    ["香車", "桂馬", "銀将", "金将", "玉将", "金将", "銀将", "桂馬", "香車"]
+  ]
 }
+// TODO: リサイズしても盤面の9x9が崩れないようにする
 // Top, 盤面とコマ置き場、色々なボタン含めた全部
 // Ban, 盤面
 // 9 8 7 6 5 4 3 2 1
-//                   ich
-//                   ni
-//                   san
-//                   shi
-//                   go
-//                   roku
-//                   nana
-//                   hachi
-//                   kyu
+//                   a: ich
+//                   b: ni
+//                   c: san
+//                   d: shi
+//                   e: go
+//                   f: roku
+//                   g: nana
+//                   h: hachi
+//                   i: kyu
 </script>
 
 <style>
