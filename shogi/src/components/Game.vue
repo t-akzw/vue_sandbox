9f7c61f497000877c1623f975858027fbe8458d8
t-akzw <45358756+t-akzw@users.noreply.github.com>
2020-08-03 09:59:26 +0900

    game profile vue add

---
+++ shogi/src/components/Game.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Top">
+  <div class="Game">Game {{ $route.params.id }}</div>
+
+  <div id="board">{{ board[1]["ich"] }}
+    <div id="itemA">A</div> <!-- アイテム -->
+    <div id="itemB">B</div> <!-- アイテム -->
+    <div id="itemC">C</div> <!-- アイテム -->
+  </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Game extends Vue {
+  board = {
+    1: {
+      ich: "香車"
+    }
+  }
+
+}
+// Top, 盤面とコマ置き場、色々なボタン含めた全部
+// Ban, 盤面
+// 9 8 7 6 5 4 3 2 1
+//                   ich
+//                   ni
+//                   san
+//                   shi
+//                   go
+//                   roku
+//                   nana
+//                   hachi
+//                   kyu
+</script>
+
+<style>
+#board {
+    display: grid; /* グリッドレイアウト */
+}
+</style>
--- shogi/src/components/User.vue
+++ shogi/src/components/User.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="User">User {{ $route.params.id }}</div>
+  <div class="User">
+    User {{ $route.params.id }}
+    <router-link :to="`${$route.params.id}/game`">aa</router-link>
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class User extends Vue {}
+export default class User extends Vue {
+
+}
 </script>
