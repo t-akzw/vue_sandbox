8fa633d828871525981a7fb28e9fe64f10d2245e
t-akzw <45358756+t-akzw@users.noreply.github.com>
2020-08-26 01:56:16 +0900

    holding 無駄コメント削除

--- shogi/src/domains/holding.ts
+++ shogi/src/domains/holding.ts
@@ -17,11 +17,9 @@ export class Holding {
   static my: Piece[];
   static op: Piece[];
   private static opHolding() {
-    // TODO: 駒のfile, rankは手動で設定せずに自動で入れたい
     this.my = [ new Lance(0, 0, true), new Lance(0, 0, false) ]
   }
   private static myHolding() {
-    // TODO: 駒のfile, rankは手動で設定せずに自動で入れたい
     this.op = [ new Pawn(0, 0, true), new Lance(0, 0, false) ]
   }
 }
